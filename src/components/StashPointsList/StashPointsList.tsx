ac1bc76c1754cb2dea128fcf56111dfa757122b4
mario <marioegymamdouh@gmail.com>
2022-04-14 19:22:04 +0200

    Add sort functionality

--- src/components/StashPointsList/StashPointsList.tsx
+++ src/components/StashPointsList/StashPointsList.tsx
@@ -4,12 +4,13 @@ import {Stashpoints} from '../../Data';
 import StashPoint from '../StashPoint/StashPoint';
 import styles from './StashPointsList.module.css'
 import {DraftCart} from '../../Data';
-import Loading from "../Loading/Loading";
+import Loading from '../Loading/Loading';
 
 interface IStashPointsListProps {
   selectedStashPointId: DraftCart['stashpointId'],
   stashPointIdChangeHandler: (id: DraftCart['stashpointId']) => void
 }
+type SortType = "default" | "rating" | "bagPerDayPrice";
 
 const StashPointsList = ({
   selectedStashPointId,
@@ -17,12 +18,31 @@ const StashPointsList = ({
 }: IStashPointsListProps) => {
   const [isLoading, setIsLoading] = useState(true);
   const [stashPoints, setStashPoints] = useState<Stashpoints>([]);
+  const [sortedStashPoints, setSortedStashPoints] = useState<Stashpoints>([]);
+  const [sortType, setSortType] = useState<SortType>("default");
 
   useEffect(() => {
     fetchStashPoints().then((stashPoints: Stashpoints) => {
       setStashPoints(stashPoints);
     })
   }, []);
+  useEffect(() => {
+    const tempSortedStashPoints = [...stashPoints];
+    switch (sortType) {
+      case "bagPerDayPrice":
+        tempSortedStashPoints.sort(
+          (a, b) =>
+            Number(Data.Price.encode(b.bagPerDayPrice)) - Number(Data.Price.encode(a.bagPerDayPrice))
+        );
+        break;
+      case "rating":
+        tempSortedStashPoints.sort((a, b) => b.rating - a.rating);
+        break;
+      case "default":
+      default:
+    }
+    setSortedStashPoints(tempSortedStashPoints);
+  }, [stashPoints, sortType]);
 
   const fetchStashPoints = async (): Promise<Stashpoints> => {
     setIsLoading(true);
@@ -41,9 +61,18 @@ const StashPointsList = ({
 
   return (
     <div>
+      <select
+        className={styles.sort}
+        value={sortType}
+        onChange={(e) => setSortType(e.target.value as SortType)}
+      >
+        <option value='default'>Default</option>
+        <option value='rating'>Rating</option>
+        <option value='bagPerDayPrice'>Bag Per Day Price</option>
+      </select>
       <ul className={styles.stashPointsList}>
         {
-          stashPoints.map(stashPoint =>
+          sortedStashPoints.map(stashPoint =>
             <StashPoint
               key={stashPoint.id}
               stashPoint={stashPoint}
